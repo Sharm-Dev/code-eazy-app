c5ba8dd0b5a788d1099b597039cde9d46d30781b
Sharmaine <sharmainetaylor@ymail.com>
2022-09-09 23:17:56 +0100

    Made changes to the end point name

---
+++ src/Pages/index/ArrayMenu.js
@@ -0,0 +1,48 @@
+
+import { useNavigate, Link } from "react-router-dom";
+ 
+
+export default function ArrayMenu() {
+  const navigate = useNavigate();
+ 
+
+
+  function arrayButtonOne() {
+    navigate("arrayQ1");
+    
+  }
+  function arrayButtonTwo() {
+    navigate("arrayQ2")
+  }
+  function arrayButtonThree() {
+    navigate("arrayQ3")
+  }
+
+  return (
+    <div>
+      <p>Question About Arrays</p>
+      <button
+        type="button"
+        className="ArrayBtns"
+        onClick={() => { 
+          arrayButtonOne()
+        }}
+      >Button one</button>
+      <button
+        type="button"
+        className="ArrayBtns"
+        onClick={() => {
+          arrayButtonTwo()
+        }}
+      >Button Two</button>
+      <button
+        type="button"
+        className="ArrayBtns"
+        onClick={()=>{arrayButtonThree()}}
+      >Button Three</button>
+      <button>
+        <Link to="/main-menu">Back</Link>
+      </button>
+    </div>
+  );
+}
